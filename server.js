8831046f36a866dc6e7a661a2b25a046fe3c0e7e
adfinem <43742265+rising-dancho@users.noreply.github.com>
2024-09-01 08:09:44 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,4 @@
+const { createRandomNumber, converToFahrenheit } = require('./utils');
+
+console.log(`Generated random number: ${createRandomNumber()}`);
+console.log(`Convert Celsius to Fahrenheit: ${converToFahrenheit(0)}`);
