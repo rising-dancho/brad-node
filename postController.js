461b73dd3ef22ce11de19c8c21411edf9a2e365e
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-09-01 08:46:09 +0800

    feat: crash course node 2024 brad

---
+++ postController.js
@@ -0,0 +1,10 @@
+const posts = [
+  { id: 1, title: 'Post one' },
+  { id: 2, title: 'Post two' },
+];
+
+function getPosts() {
+  return posts;
+}
+
+export default getPosts;
--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-const { createRandomNumber, converToFahrenheit } = require('./utils');
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< //
+//                           ES6 syntax                          //
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< //
+import getPosts from './postController.js';
 
-console.log(`Generated random number: ${createRandomNumber()}`);
-console.log(`Convert Celsius to Fahrenheit: ${converToFahrenheit(0)}`);
+console.log(getPosts());
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< //
+//                           common js version                   //
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< //
+// const { createRandomNumber, converToFahrenheit } = require('./utils');
+
+// console.log(`Generated random number: ${createRandomNumber()}`);
+// console.log(`Convert Celsius to Fahrenheit: ${converToFahrenheit(0)}`);
