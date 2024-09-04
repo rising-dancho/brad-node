40c197b773e4ee164882f7f07c4f422a02463371
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-09-05 03:14:30 +0800

    feat: refactored routes into middlewares

--- server.js
+++ server.js
@@ -7,50 +7,62 @@ const users = [
   { id: 3, name: 'tang tong' },
 ];
 
+// Logger middleware
 function logger(req, res, next) {
   console.log(`${req.method} ${req.url}`);
   next();
 }
 
+// JSON middleware
+function jsonMiddleware(req, res, next) {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  next();
+}
+
+// Route handler for GET /
+function rootHandler(req, res) {
+  res.end(JSON.stringify({ message: 'Welcome bro' }));
+}
+
+// Route handler for GET /api/users
+function getUsersHandler(req, res) {
+  res.end(JSON.stringify(users));
+}
+
+// Route handler for GET /api/users/:id
+function getUserByIDHandler(req, res) {
+  const id = req.url.split('/')[4];
+  const user = users.find((user) => user.id === parseInt(id));
+
+  if (user) {
+    res.end(JSON.stringify(user));
+  } else {
+    res.end(JSON.stringify({ message: 'User does not exist' }));
+  }
+}
+
+// Route handler for User Not Found
+function notFoundHandler(req, res) {
+  res.write(404, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ message: 'User does not exist' }));
+}
+
 const server = createServer((req, res) => {
   logger(req, res, function next() {
-    if (req.url === '/' && req.method === 'GET') {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(
-        '<h1>Welcome bro</h1> <img src="https://p16-tm-sg.tiktokmusic.me/img/tos-alisg-v-2102/1c005cfb6b21488e8b499241530e364a~c5_500x500.image" alt="welcome bro">'
-      );
-    } else if (req.url === '/api/v1/users' && req.method === 'GET') {
-      try {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(users));
-      } catch (error) {
-        res.writeHead(500, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: error }));
-      }
-    } else if (
-      req.url.match(/\/api\/v1\/users\/([0-9]+)/) &&
-      req.method === 'GET'
-    ) {
-      const id = req.url.split('/')[4];
-      const user = users.find((user) => user.id === parseInt(id));
-      // console.log('id:', id);
-
-      if (user) {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(user));
+    jsonMiddleware(req, res, function next() {
+      if (req.url === '/' && req.method === 'GET') {
+        rootHandler(req, res);
+      } else if (req.url === '/api/v1/users' && req.method === 'GET') {
+        getUsersHandler(req, res);
+      } else if (
+        req.url.match(/\/api\/v1\/users\/([0-9]+)/) &&
+        req.method === 'GET'
+      ) {
+        getUserByIDHandler(req, res);
       } else {
-        res.write(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>User does not exist</h1>');
+        notFoundHandler(req, res);
       }
-      try {
-      } catch (error) {
-        res.writeHead(500, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: error }));
-      }
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('<h1>404 Page not found</h1>');
-    }
+    });
   });
 });
 
